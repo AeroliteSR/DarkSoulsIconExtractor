1b58ca1b59eb7a2dbefd9f52c6facd62766ae411
Aero <164753612+dash512@users.noreply.github.com>
2026-08-31 20:41:33 +0100

    version bump

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ if sys.platform == "win32":
 
 setup(
     name="DSTS",
-    version="3.10.2",
+    version="3.10.3",
     description="Dark Souls Texture Studio",
     options={"build_exe": build_exe_options},
     cmdclass={"build_exe": CustomBuild},
